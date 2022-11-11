9eb4bea6281a723f4673de64ed3c0b68da8c59da
efimov-top-academy <efimov_m@t.top-academy.ru>
2022-11-11 16:25:38 +0300

    Добавьте файлы проекта.

---
+++ WpfControlsApp/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfControlsApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            btn.Background = new SolidColorBrush(Colors.Green);
+            btn.FontSize = 30;
+            btn.Content = "New Content";
+
+            btn.Margin = new Thickness();
+
+            Button btn3 = new() { Content = "AAAAAA" };
+            stack.Children.Add(btn3);
+        }
+    }
+}
